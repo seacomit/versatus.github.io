0eddacdc6fa51642625c4350d2e47f882acb452e
versatus <86527888+versatus@users.noreply.github.com>
2023-03-22 18:48:25 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+   const colorPallete = {};
+   colorPallete.background = "#263c41";
+
+
+   document.body.style = `background-color: ${colorPallete.background}; padding: 0; margin: 0;`;
+
+});
